0db0bc2003fec5544f2b0aed55b9333215472d88
Guilherme Diniz <guilhermedinizlau@gmail.com>
2025-08-13 23:54:21 -0300

    adiciona script de backup do banco de dados locadora

---
+++ banco/locadora.sql
@@ -0,0 +1,176 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 13, 2025 at 11:53 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `locadora`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `id_categoria` int(2) NOT NULL,
+  `descricao` varchar(20) NOT NULL,
+  `valor` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `categoria`
+--
+
+INSERT INTO `categoria` (`id_categoria`, `descricao`, `valor`) VALUES
+(1, 'SUV', 200),
+(2, 'Passeio', 100),
+(3, 'Van', 270),
+(6, 'Off Road', 312),
+(7, 'Blindado', 231);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exemplares`
+--
+
+CREATE TABLE `exemplares` (
+  `id_exemplar` int(11) NOT NULL,
+  `placa_veiculo` varchar(8) NOT NULL,
+  `id_locacao` int(11) NOT NULL,
+  `locado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `locacao`
+--
+
+CREATE TABLE `locacao` (
+  `id_locacao` int(11) NOT NULL,
+  `data` date NOT NULL,
+  `valor_total` float NOT NULL,
+  `cpf_socio` varchar(11) NOT NULL,
+  `id_veiculo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `socios`
+--
+
+CREATE TABLE `socios` (
+  `cpf` varchar(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `rg` varchar(12) NOT NULL,
+  `endereco` varchar(100) NOT NULL,
+  `telefone` varchar(12) NOT NULL,
+  `email` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `user` text DEFAULT NULL,
+  `senha` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `veiculos`
+--
+
+CREATE TABLE `veiculos` (
+  `placa` varchar(8) NOT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `anoFabricacao` int(10) UNSIGNED DEFAULT NULL,
+  `fabricante` varchar(30) DEFAULT NULL,
+  `opcionais` text DEFAULT NULL,
+  `motorizacao` varchar(50) NOT NULL,
+  `valorBase` float NOT NULL,
+  `id_categoria` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id_categoria`);
+
+--
+-- Indexes for table `exemplares`
+--
+ALTER TABLE `exemplares`
+  ADD PRIMARY KEY (`id_exemplar`);
+
+--
+-- Indexes for table `locacao`
+--
+ALTER TABLE `locacao`
+  ADD PRIMARY KEY (`id_locacao`);
+
+--
+-- Indexes for table `socios`
+--
+ALTER TABLE `socios`
+  ADD PRIMARY KEY (`cpf`);
+
+--
+-- Indexes for table `veiculos`
+--
+ALTER TABLE `veiculos`
+  ADD PRIMARY KEY (`placa`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id_categoria` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `exemplares`
+--
+ALTER TABLE `exemplares`
+  MODIFY `id_exemplar` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `locacao`
+--
+ALTER TABLE `locacao`
+  MODIFY `id_locacao` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
