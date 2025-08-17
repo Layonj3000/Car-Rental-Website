8b2aa1e8950791d39853c5c328a4845ec41e0fc9
Layonj3000 <layon.reis@edu.ufes.br>
2025-08-16 21:20:54 -0300

    bug: ajuste no consultar locação

--- controlers/controlerLocacao.php
+++ controlers/controlerLocacao.php
@@ -1,5 +1,4 @@
 <?php
-    include_once "../model/locacao.inc.php";
     include_once "../dao/locacaoDao.inc.php";
     include_once "../utils/funcoesUteis.php";
     session_start();
@@ -21,15 +20,17 @@
         header("Location: ../views/locacoes/visualizacaoLocacoes.php");
     }
 
-    if($op == 2){//selecionar todos
-        $locacaoDao = new locacaoDao();
-
-        $data1 = (string)(isset($_REQUEST['dataInicial'])? $_REQUEST['dataInicial']:'');
-        $data2 = (string)(isset($_REQUEST['dataFinal'])? $_REQUEST['dataFinal']:'');
-
-        $locacoes = $locacaoDao -> getLocacoesPorPeriodo($data1, $data2);
-
-        $_SESSION['locacoes'] = $locacoes;
+    if($op == 2){ 
+        $dataInicial = isset($_REQUEST['dataInicial']) ? $_REQUEST['dataInicial'] : '';
+        $dataFinal = isset($_REQUEST['dataFinal']) ? $_REQUEST['dataFinal'] : '';
+        
+        
+        if(!empty($dataInicial) || !empty($dataFinal)){
+            $_SESSION['locacoes'] = $dao->getLocacoesPorPeriodo(converteTimestamp($dataInicial), converteTimestamp($dataFinal));
+        } else {
+            $_SESSION['locacoes'] = $dao->getLocacoes();
+        }
 
         header("Location: ../views/locacoes/visualizacaoLocacoes.php");
     }
+?>
--- utils/funcoesUteis.php
+++ utils/funcoesUteis.php
@@ -1,12 +1,20 @@
 <?php
 
-function formatarData($data)
-{
-    return date('d/m/Y',$data);
-}
+    function formatarData($data)
+    {
+        return date('d/m/Y',$data);
+    }
 
-function converteDataMySql($data){
-    return date('Y-m-d', $data);
-}
-
+    function converteDataMySql($data){
+        return date('Y-m-d', $data);
+    }
+    function converteTimestamp($data){
+        if (!empty($data)) {
+            $timestamp = strtotime($data);
+            if ($timestamp !== false) {
+                return date('Y-m-d', $timestamp);
+            }
+        }
+        return null;
+    }
 ?>
