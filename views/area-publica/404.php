d048fec40ae1f08003e416f9859ef0644bc9c540
Layonj3000 <layon.reis@edu.ufes.br>
2025-08-17 18:53:15 -0300

    bug: ajuste no error 404

--- views/area-publica/404.php
+++ views/area-publica/404.php
@@ -1,6 +1,6 @@
 <?php
-// Define o cabeçalho HTTP para o erro 404
-header("HTTP/1.0 404 Not Found");
+    http_response_code(404);
+    $base = str_replace('\\','/', dirname($_SERVER['SCRIPT_NAME'])) . '/';
 ?>
 
 <!DOCTYPE html>
@@ -8,7 +8,7 @@ header("HTTP/1.0 404 Not Found");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../estilos/estilo-404.css">
+    <link rel="stylesheet" href="<?php echo $base; ?>../../estilos/estilo-404.css">
     <title>Página não encontrada - Erro 404</title>
 </head>
 <body>
@@ -16,9 +16,9 @@ header("HTTP/1.0 404 Not Found");
         <h1>Ops! Página não encontrada</h1>
         <p>Parece que o pneu furou e a página que você estava procurando não existe. Mas não se preocupe, temos outros veículos esperando por você!</p>
         <div class="links">
-            <a href="../../views/area-publica/index.php">Ir para a Página Inicial</a>
-            <a href="../../views/area-publica/showRoom.php">Ver Nossa Frota de Carros</a>
-            <a href="../../views/area-publica/fale-conosco.php">Fale Conosco</a>
+            <a href="<?php echo $base; ?>../../views/area-publica/index.php">Ir para a Página Inicial</a>
+            <a href="<?php echo $base; ?>../../views/area-publica/showRoom.php">Ver Nossa Frota</a>
+            <a href="<?php echo $base; ?>../../views/area-publica/fale-conosco.php">Fale Conosco</a>
         </div>
     </div>
     <div class="credit">
