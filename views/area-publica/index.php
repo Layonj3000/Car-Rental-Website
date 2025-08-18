bfeb18c257df84787721728d7c6ce454446c7d6a
Layonj3000 <layon.reis@edu.ufes.br>
2025-08-18 14:38:20 -0300

    style: colocando banner na home

--- views/area-publica/index.php
+++ views/area-publica/index.php
@@ -7,6 +7,10 @@
 <section class="my-5">
     <div class="container">
         <div class="row">
+            <div class="col-12 mb-4">
+                <img src="../../imagens/bannercar.png" alt="Banner da Locadora de Veículos Velocity" class="img-fluid banner-image">
+            </div>
+            
             <div class="col-md-8 mx-auto fs-5" align="justify">
                 <p>Procurando a melhor opção para a sua próxima viagem, passeio ou para o dia a dia? Na <strong>Velocity</strong>, você encontra a frota perfeita para a sua necessidade. Carros novos, revisados e com a qualidade que você merece, tudo isso com a facilidade de fazer a sua reserva online!</p>
                 <p>Na <strong>Velocity</strong>, a experiência de alugar um carro é única. Você pode escolher seu veículo, verificar a disponibilidade, fazer a reserva e até mesmo o pagamento diretamente pelo nosso site. Oferecemos as melhores condições de aluguel e diversas opções de pagamento, incluindo PIX, cartão de débito ou crédito. Alugue do seu jeito, com total flexibilidade e segurança!</p>
